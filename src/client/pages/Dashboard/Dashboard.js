243c373a87d8c91f9611c947b85201db464f3ec5
Frantz Kati <bahdcoder@gmail.com>
2020-06-08 16:57:38 +0100

    Basic dashboard setup

--- src/client/pages/Dashboard/Dashboard.js
+++ src/client/pages/Dashboard/Dashboard.js
@@ -5,7 +5,22 @@ import { mustBeAuthenticated } from '../../store/auth'
 
 class DashboardPage extends React.Component {
     render() {
-        return <div>this is the dashboard</div>
+        return (
+            <div className="w-full">
+                <div className="w-1/6 h-screen">
+                    <div className="w-full h-12 bg-blue-500">
+
+                    </div>
+                    <div className="w-full bg-blue-900" style={{ height: `calc(100vh - 3rem)` }}>
+
+                    </div>
+                </div>
+
+                <div className="w-5/6">
+
+                </div>
+            </div>
+        )
     }
 }
 
