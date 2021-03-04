ec5f49450355b59c8f4fa6a9763f4cf0fda7c2ec
Kati Frantz <bahdcoder@gmail.com>
2021-03-04 22:04:20 +0100

    v0.7.19

--- packages/media/server/type-defs.ts
+++ packages/media/server/type-defs.ts
@@ -1,14 +1,16 @@
-import { gql } from 'apollo-server-express'
+export const typeDefs = (name: string, plural: string) => {
+    const { gql } = require('apollo-server-express')
 
-export const typeDefs = (name: string, plural: string) => gql`
-    scalar Upload
+    return gql`
+        scalar Upload
 
-    input upload_files_input {
-        ${plural}: [Upload]!
-        path: String
-    }
+        input upload_files_input {
+            ${plural}: [Upload]!
+            path: String
+        }
 
-    extend type Mutation {
-        upload_files(object: upload_files_input!): [${name}]!
-    }
-`
+        extend type Mutation {
+            upload_files(object: upload_files_input!): [${name}]!
+        }
+    `
+}
