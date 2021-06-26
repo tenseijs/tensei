3272bfd48dc761812c3dba05f0d6e06055262abd
Kati Frantz <bahdcoder@gmail.com>
2021-06-26 22:00:18 +0100

    chore(release): publish v0.8.7

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,5 @@ set -e
 
 yarn before:release "$@"
 git add .
-git commit -m "chore(release): publish `node -p 'require(\"./lerna.json\").version'`"
-# yarn lerna publish "$@"
+git commit -m "chore(release): publish v`node -p 'require(\"./lerna.json\").version'`"
+yarn lerna publish "$@"
