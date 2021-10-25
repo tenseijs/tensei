b9970885bab4bec1a5020ca332fd7e92de37b2cd
Kati Frantz <bahdcoder@gmail.com>
2021-10-25 12:37:43 +0100

    chore(cms): rename home to dashboard

--- packages/cms/pages/components/dashboard/routes/routes.tsx
+++ packages/cms/pages/components/dashboard/routes/routes.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-import { Home } from '../../../home'
+import { Dashboard } from '../../../dashboard'
 import { Resource } from '../../../resources/resource'
 
 export const DashboardRoutes: React.FunctionComponent = () => {
   return (
     <>
       <Route
+        exact
         component={Resource}
         path={window.Tensei.getPath('resources/:resource')}
       />
-      <Route exact component={Home} path={window.Tensei.getPath('')} />
+      <Route exact component={Dashboard} path={window.Tensei.getPath('')} />
     </>
   )
 }
--- packages/cms/pages/dashboard/dashboard.tsx
+++ packages/cms/pages/dashboard/dashboard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { DashboardLayout } from '../components/dashboard/layout'
 
-export const Home: React.FunctionComponent = () => {
+export const Dashboard: React.FunctionComponent = () => {
   return <DashboardLayout></DashboardLayout>
 }
