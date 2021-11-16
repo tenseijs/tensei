0abf898e5638f0b779c3a9d69249eb830b3a4b40
Ebuka <ebukaodini@gmail.com>
2021-11-16 18:05:43 +0100

    Feat/user account registration (#182)
    * fix: changed response error type fromError to AxiosError
    
    * fix: exported AuthMethods and updated RegisterCredentials
    
    * feat: completed the User account registration #167
    
    * fix: allow empty
    
    * fix: implemented changes as requested by @bahdcoder
    
    * fix: cleanup code
    
    * fix: applied changes as requested by @bahdcoder and fixed the submit loader not showing issue

--- packages/cms/pages/auth/register/register.tsx
+++ packages/cms/pages/auth/register/register.tsx
@@ -1,23 +1,65 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 import styled from 'styled-components'
 import { AuthLayout } from '../../components/auth/layout'
 
 import { EuiText } from '@tensei/eui/lib/components/text'
 import { EuiLink } from '@tensei/eui/lib/components/link'
 import { EuiTitle } from '@tensei/eui/lib/components/title'
-import { EuiFormRow } from '@tensei/eui/lib/components/form'
+import { EuiForm, EuiFormRow } from '@tensei/eui/lib/components/form'
 import { EuiButton } from '@tensei/eui/lib/components/button'
 import { EuiSpacer } from '@tensei/eui/lib/components/spacer'
 import { EuiFlexGroup } from '@tensei/eui/lib/components/flex'
 import { EuiFieldText } from '@tensei/eui/lib/components/form/field_text'
 import { EuiFieldPassword } from '@tensei/eui/lib/components/form/field_password'
 import { EuiFlexItem } from '@tensei/eui/lib/components/flex/flex_item'
+import { useState } from 'react'
+import { RegisterCredentials, useAuthStore } from '../../../store/auth'
 
 const H3 = styled.h3`
   text-align: center;
 `
 
+// Define errors type
+type RegisterErrors = Partial<Record<keyof RegisterCredentials, string[]>>
+
 export const Register: React.FunctionComponent = () => {
+  const { register } = useAuthStore()
+
+  const [user, setUser] = useState<RegisterCredentials>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  })
+
+  // declare page states
+  const [errors, setErrors] = useState<RegisterErrors>({})
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const onSubmit = async (submitEvent: FormEvent<HTMLFormElement>) => {
+    submitEvent.preventDefault()
+    setIsSubmitting(true)
+    const [response, error] = await register(user)
+
+    if (error) {
+      let errorData = error.response?.data?.errors
+
+      let errors: RegisterErrors = {}
+
+      // get error messages
+      errorData.forEach((error: { message: string; field: string }) => {
+        errors[error.field as keyof RegisterErrors] = [error.message]
+      })
+
+      setErrors({ ...errors })
+      setIsSubmitting(false)
+      return
+    }
+
+    window.location.href = window.Tensei.getPath('')
+    setIsSubmitting(false)
+  }
+
   return (
     <AuthLayout>
       <EuiTitle size="s">
@@ -31,36 +73,84 @@ export const Register: React.FunctionComponent = () => {
 
       <EuiSpacer size="xl" />
 
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <EuiFormRow label="First name">
-            <EuiFieldText autoFocus />
+      <form method="post" onSubmit={onSubmit} noValidate>
+        <EuiForm component="div">
+          <EuiFlexGroup>
+            <EuiFlexItem>
+              <EuiFormRow
+                label="First name"
+                isInvalid={errors?.firstName && true}
+                error={errors?.firstName}
+              >
+                <EuiFieldText
+                  autoFocus
+                  onChange={changeEvent => {
+                    errors.firstName = undefined
+                    setUser({ ...user, firstName: changeEvent.target.value })
+                  }}
+                  isInvalid={errors?.firstName && true}
+                />
+              </EuiFormRow>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiFormRow
+                label="Last name"
+                isInvalid={errors?.lastName && true}
+                error={errors?.lastName}
+              >
+                <EuiFieldText
+                  onChange={changeEvent => {
+                    errors.lastName = undefined
+                    setUser({ ...user, lastName: changeEvent.target.value })
+                  }}
+                  isInvalid={errors?.lastName && true}
+                />
+              </EuiFormRow>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+
+          <EuiSpacer size="l" />
+
+          <EuiFormRow
+            label="Email"
+            isInvalid={errors?.email && true}
+            error={errors?.email}
+          >
+            <EuiFieldText
+              fullWidth
+              onChange={changeEvent => {
+                errors.email = undefined
+                setUser({ ...user, email: changeEvent.target.value })
+              }}
+              isInvalid={errors?.email && true}
+            />
           </EuiFormRow>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiFormRow label="Last name">
-            <EuiFieldText />
+
+          <EuiSpacer size="l" />
+
+          <EuiFormRow
+            label="Password"
+            isInvalid={errors?.password && true}
+            error={errors?.password}
+          >
+            <EuiFieldPassword
+              type="dual"
+              fullWidth
+              onChange={changeEvent => {
+                errors.password = undefined
+                setUser({ ...user, password: changeEvent.target.value })
+              }}
+              isInvalid={errors?.password && true}
+            />
           </EuiFormRow>
-        </EuiFlexItem>
-      </EuiFlexGroup>
 
-      <EuiSpacer size="l" />
+          <EuiSpacer size="l" />
 
-      <EuiFormRow label="Email">
-        <EuiFieldText fullWidth />
-      </EuiFormRow>
-
-      <EuiSpacer size="l" />
-
-      <EuiFormRow label="Password">
-        <EuiFieldPassword type="dual" fullWidth />
-      </EuiFormRow>
-
-      <EuiSpacer size="l" />
-
-      <EuiButton fullWidth fill>
-        Get started
-      </EuiButton>
+          <EuiButton fullWidth fill type="submit" isLoading={isSubmitting}>
+            Get started
+          </EuiButton>
+        </EuiForm>
+      </form>
 
       <EuiSpacer size="m" />
       <EuiText size="xs" textAlign="center">
