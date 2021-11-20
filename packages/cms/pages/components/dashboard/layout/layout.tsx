66eb77a1825f674e1739bc3aea67ed1ad3386cb0
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2021-11-20 19:57:39 +0100

    feat: Collapsible navigation menu (#189)
    Co-authored-by: Kati Frantz <bahdcoder@gmail.com>

--- packages/cms/pages/components/dashboard/layout/layout.tsx
+++ packages/cms/pages/components/dashboard/layout/layout.tsx
@@ -10,6 +10,8 @@ import { EuiButton } from '@tensei/eui/lib/components/button'
 import { EuiButtonEmpty } from '@tensei/eui/lib/components/button/button_empty'
 import { AvatarContextMenu } from './avatar-context-menu'
 
+import { SidebarMenu } from './sidebar'
+
 const Sidebar = styled.div<{
   bg?: string
 }>`
@@ -21,27 +23,6 @@ const Sidebar = styled.div<{
   border-right: ${({ theme }) => theme.border.thin};
 `
 
-const NestedSidebar = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 260px;
-  height: 100%;
-  position: relative;
-  border-right: ${({ theme }) => theme.border.thin};
-`
-
-const NestedSidebarHeader = styled.div`
-  height: 75px;
-  padding: 0 1.75rem;
-  display: flex;
-  align-items: center;
-`
-
-const NestedSidebarTitleUnderline = styled.div`
-  width: 25%;
-  margin: 0 1.75rem;
-  ${({ theme }) => `border-bottom: ${theme.border.thin}`}
-`
 
 const SidebarWrapper = styled.div`
   display: flex;
@@ -68,24 +49,6 @@ const GroupName = styled(EuiText)`
   font-weight: 500;
   text-transform: uppercase;
   ${({ theme }) => `color: ${theme.colors.subdued}`}
-`
-
-const NestedSidebarGroupName = styled(EuiText)`
-  font-size: 11px;
-  font-weight: 500;
-  padding: 0rem 1.75rem;
-  text-transform: uppercase;
-  ${({ theme }) => `color: ${theme.colors.subdued}`}
-`
-
-const NestedSidebarNavItem = styled.li`
-  width: 100%;
-`
-
-const Group = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
 `
 
 const SidebarContainer = styled.div`
@@ -161,20 +124,6 @@ const Content = styled.div`
   overflow-x: hidden;
 `
 
-const CollapseExpandIcon = styled.button`
-  width: 28px;
-  height: 28px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-  position: absolute;
-  border: ${({ theme }) => theme.border.thin};
-
-  top: 23.5px;
-  right: -14px;
-  background-color: ${({ theme }) => theme.colors.ghost};
-`
 
 const TitleAndBackButtonContainer = styled.div`
   display: flex;
@@ -221,23 +170,7 @@ export const DashboardLayout: React.FunctionComponent = ({ children }) => {
             <AvatarContextMenu />
           </Footer>
         </Sidebar>
-        <NestedSidebar>
-          <CollapseExpandIcon>
-            <EuiIcon size="s" type="arrowLeft" />
-          </CollapseExpandIcon>
-          <NestedSidebarHeader>
-            <EuiTitle size="s">
-              <h1>Content</h1>
-            </EuiTitle>
-          </NestedSidebarHeader>
-          <NestedSidebarTitleUnderline />
-
-          <EuiSpacer size="l" />
-
-          <Group>
-            <NestedSidebarGroupName>Resources</NestedSidebarGroupName>
-          </Group>
-        </NestedSidebar>
+        <SidebarMenu />
       </SidebarWrapper>
       <Body>
         <Topbar>
