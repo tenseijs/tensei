6b46f717672f166d2fae6b6c6fd1d4e711f24654
Anisat Akinbani <anisatakinbani13@gmail.com>
2021-11-23 08:49:53 +0100

    feat: Sidebar menu contents

--- packages/cms/pages/components/dashboard/layout/sidebar.tsx
+++ packages/cms/pages/components/dashboard/layout/sidebar.tsx
@@ -6,13 +6,13 @@ import { EuiText } from '@tensei/eui/lib/components/text'
 import { EuiTitle } from '@tensei/eui/lib/components/title'
 import { EuiSpacer } from '@tensei/eui/lib/components/spacer'
 
-const NestedSidebar = styled.div<{close: boolean}>`
+const NestedSidebar = styled.div<{ close: boolean }>`
   display: flex;
   flex-direction: column;
-  width: ${({close}) => close ? '0' : '260px'};
+  width: ${({ close }) => (close ? '0' : '260px')};
   height: 100%;
   position: relative;
-  border-right: ${({ theme, close }) => close ? 'none': theme.border.thin};
+  border-right: ${({ theme, close }) => (close ? 'none' : theme.border.thin)};
 `
 
 const CollapseExpandIcon = styled.button`
@@ -29,10 +29,10 @@ const CollapseExpandIcon = styled.button`
   background-color: ${({ theme }) => theme.colors.ghost};
 `
 
-const NestedSidebarHeader = styled.div<{close: boolean}>`
+const NestedSidebarHeader = styled.div<{ close: boolean }>`
   height: 75px;
   padding: 0 1.75rem;
-  display: ${({close}) => close ? 'none' : 'flex'};
+  display: ${({ close }) => (close ? 'none' : 'flex')};
   align-items: center;
 `
 const NestedSidebarTitleUnderline = styled.div`
@@ -41,9 +41,9 @@ const NestedSidebarTitleUnderline = styled.div`
   ${({ theme }) => `border-bottom: ${theme.border.thin}`}
 `
 
-const Group = styled.div<{close: boolean}>`
+const Group = styled.div<{ close: boolean }>`
   width: 100%;
-  display: ${({close}) => close ? 'none' : 'flex'};
+  display: ${({ close }) => (close ? 'none' : 'flex')};
   flex-direction: column;
 `
 
@@ -51,38 +51,101 @@ const NestedSidebarGroupName = styled(EuiText)`
   font-size: 11px;
   font-weight: 500;
   padding: 0rem 1.75rem;
+  margin-bottom: 0.75rem;
   text-transform: uppercase;
   ${({ theme }) => `color: ${theme.colors.subdued}`}
 `
+const NavItem = styled.div`
+  display: block;
+  width: 230px;
+  height: 38px;
+`
 
+const NavItemWrapper = styled.li<{
+  active?: boolean
+}>`
+  display: flex;
+  align-items: center;
+  padding: 0.65rem;
+  margin-left: 1.5rem;
+  margin-right: 1.5rem;
+  margin-bottom: 0.3rem;
+  list-style: none;
+  
+  ${({ active, theme }) =>
+    active
+      ? `
+    background-color: ${theme.colors.primaryTransparent};
+    color: ${theme.colors.primaryText};
+    border-radius: 5px;
+    `
+      : ``}
+`
 
 export const SidebarMenu: React.FunctionComponent = () => {
-    const [close, setClose] = useState(false)
+  const [close, setClose] = useState(false)
 
-    const onCloseSideBar = () => setClose(!close)
+  const onCloseSideBar = () => setClose(!close)
 
-    return (
-      <NestedSidebar close={close}>
-        <CollapseExpandIcon onClick={onCloseSideBar}>
-          {close ? 
-            <EuiIcon size="s" type="arrowRight" />
-              :
-            <EuiIcon size="s" type="arrowLeft" />
-          }
-        </CollapseExpandIcon>
-        <NestedSidebarHeader close={close}>
-          <EuiTitle size="s">
-            <h1>Content</h1>
-          </EuiTitle>
-        </NestedSidebarHeader>
-        <NestedSidebarTitleUnderline />
+  return (
+    <NestedSidebar close={close}>
+      <CollapseExpandIcon onClick={onCloseSideBar}>
+        {close ? (
+          <EuiIcon size="s" type="arrowRight" />
+        ) : (
+          <EuiIcon size="s" type="arrowLeft" />
+        )}
+      </CollapseExpandIcon>
+      <NestedSidebarHeader close={close}>
+        <EuiTitle size="s">
+          <h1>Content</h1>
+        </EuiTitle>
+      </NestedSidebarHeader>
+      <NestedSidebarTitleUnderline />
 
-          <EuiSpacer size="l" />
+      <EuiSpacer size="l" />
 
-        <Group close={close}>
-          <NestedSidebarGroupName>Resources</NestedSidebarGroupName>
-        </Group>
-      </NestedSidebar>
+      <Group close={close}>
+        <NestedSidebarGroupName>Resources</NestedSidebarGroupName>
+        <NavItem>
+          <NavItemWrapper>
+            <EuiText size="xs">Post</EuiText>
+          </NavItemWrapper>
+        </NavItem>
 
-    )
+        <NavItem>
+          <NavItemWrapper>
+            <EuiText size="xs">Tag</EuiText>
+          </NavItemWrapper>
+        </NavItem>
+
+        <NavItem>
+          <NavItemWrapper>
+            <EuiText size="xs">Comment</EuiText>
+          </NavItemWrapper>
+        </NavItem>
+      </Group>
+      
+      <Group close={close}>
+        <NestedSidebarGroupName>Store</NestedSidebarGroupName>
+        <NavItem>
+          <NavItemWrapper active>
+            <EuiText size="xs">Orders</EuiText>
+          </NavItemWrapper>
+        </NavItem>
+
+        <NavItem>
+          <NavItemWrapper>
+            <EuiText size="xs">Shipping</EuiText>
+          </NavItemWrapper>
+        </NavItem>
+
+        <NavItem>
+          <NavItemWrapper>
+            <EuiText size="xs">Payments</EuiText>
+          </NavItemWrapper>
+        </NavItem>
+      </Group>
+    </NestedSidebar>
+  )
 }
