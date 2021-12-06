81bb4cf3dec99c01a395bea5c38e3632886116e6
Ebuka Odini <ebukaodini@gmail.com>
2021-12-07 00:54:25 +0100

    feat: added edit resource route

--- packages/cms/pages/components/dashboard/routes/routes.tsx
+++ packages/cms/pages/components/dashboard/routes/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import { Dashboard } from '../../../dashboard'
-import { Resource, CreateResource } from '../../../resources'
+import { Resource, CreateResource, UpdateResource } from '../../../resources'
 import { MustBeAuthComponent } from '../../auth/guards/must-be-authenticated'
 
 export const DashboardRoutes: React.FunctionComponent = () => {
@@ -17,6 +17,11 @@ export const DashboardRoutes: React.FunctionComponent = () => {
         exact
         component={MustBeAuthComponent(CreateResource)}
         path={window.Tensei.getPath('resources/:resource/create')}
+      />
+      <Route
+        exact
+        component={MustBeAuthComponent(UpdateResource)}
+        path={window.Tensei.getPath('resources/:resource/:id/edit')}
       />
       <Route
         exact
