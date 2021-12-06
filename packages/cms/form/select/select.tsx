0a17c7882b7711b016fd730c78c292935e96cf62
malik <malikoabdulaziz@gmail.com>
2021-12-06 14:51:08 +0100

    refactor: change select component to super select component

--- packages/cms/form/select/select.tsx
+++ packages/cms/form/select/select.tsx
@@ -1,31 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FormComponentProps } from '@tensei/components'
-import { EuiSelect } from '@tensei/eui/lib/components/form'
+import { EuiSuperSelect } from '@tensei/eui/lib/components/form'
 
 const FormSelect: React.FC<FormComponentProps> = ({
   field,
   name,
   id,
-  value,
-  onChange,
   onFocus,
   error
 }) => {
-  const options = field?.selectOptions?.map(option => {
-    return { text: option.label, value: option.value }
-  })
+  const options =
+    field?.selectOptions?.map(option => {
+      return { value: option.label, inputDisplay: option.value }
+    }) || []
+  const [value, setValue] = useState(options[1].value)
+
+  const onChange = (value: string) => {
+    setValue(value)
+  }
 
   return (
-    <EuiSelect
+    <EuiSuperSelect
       id={id}
       options={options}
       name={name}
       fullWidth
-      value={value}
+      valueOfSelected={value}
       onFocus={onFocus}
       isInvalid={!!error}
       placeholder={field.name}
-      onChange={event => onChange(event.target.value)}
+      onChange={value => onChange(value)}
       {...field.attributes}
       aria-label={field.name}
     />
