d64df00dcb7e358616cd336755f95d25e01c5efd
Anisat Akinbani <anisatakinbani13@gmail.com>
2021-12-09 19:27:00 +0100

    Modified Json editor

--- packages/cms/form/json/json.tsx
+++ packages/cms/form/json/json.tsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Controlled as CodeMirror } from 'react-codemirror2'
-import { IControlledCodeMirror } from 'react-codemirror2'
 
-const FormJson: React.FC<IControlledCodeMirror> = ({
+import { FormComponentProps } from '@tensei/components'
+import styled from 'styled-components'
+
+import { EuiFieldText } from '@tensei/eui/lib/components/form'
+
+const JsonComponent = styled.div``
+
+const FormJson: React.FC<FormComponentProps> = ({
+  field,
+  name,
+  id,
   value,
-  options,
-  onBeforeChange
+  onChange,
+  onFocus,
+  error
 }) => {
+  
     return (
-      <CodeMirror 
-        value={value}
-        options={options}
-        onBeforeChange={onBeforeChange}
-      />
+      <CodeMirror
+          value={value}
+          options={{
+            mode: 'json'
+          }}
+          onBeforeChange={(editor, data, value) => {}}
+          {...field.attributes}
+        />
     )
   }
   
