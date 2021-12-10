8afabbb153d0d49e403317bea5319935ad7a1b4c
Ebuka Odini <ebukaodini@gmail.com>
2021-12-10 17:03:41 +0100

    feat: added the boolean index component

---
+++ packages/cms/index/boolean/boolean.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { EuiHealth } from '@tensei/eui/lib/components/health'
+import { IndexComponentProps } from '@tensei/components'
+import { EuiBadge } from '@tensei/eui/lib/components/badge'
+
+const IndexBoolean: React.FC<IndexComponentProps> = ({
+  value,
+  field,
+  values,
+  resource
+}) => {
+  const color = value ? 'success' : 'danger'
+  const text = value ? field.trueLabel : field.falseLabel
+  return (
+    <EuiHealth color={color} textSize="m">
+      {text}
+    </EuiHealth>
+  )
+}
+
+export default IndexBoolean
