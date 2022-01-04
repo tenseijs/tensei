45419b553bcb7af0b7b2e9082ac7360077d8766e
Kati Frantz <bahdcoder@gmail.com>
2022-01-04 10:27:29 +0100

    chore: cleanup test examples repo

--- badoo/server.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from '@tensei/auth'
-import { graphql } from '@tensei/graphql'
-import { welcome, tensei, cors } from '@tensei/core'
-
-export default tensei()
-  .root(__dirname)
-  .plugins([welcome(), auth().plugin(), graphql().plugin(), cors()])
-  .start()
-  .catch(console.error)
