d0c21abf2ced3931a2eb608bf2e4a7479f568940
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2022-01-12 11:35:15 +0100

    feat: a user can logout of the cms dashboard (#233)

--- packages/cms/pages/components/dashboard/layout/sidebar.tsx
+++ packages/cms/pages/components/dashboard/layout/sidebar.tsx
@@ -221,6 +221,23 @@ function Landscape() {
   )
 }
 
+function Exit() {
+  return(
+    <svg
+      width={16}
+      height={16}
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path 
+        d="M12.535,12.4927 C12.792,12.4927 13.003,12.7027 13.003,12.9607 L13.003,15.5247 C13.003,15.7827 12.795,15.9997 12.537,15.9997 L12.497,15.9997 L3,15.9997 L3,-0.0003 L12.575,-0.0003 L12.595,0.0007 C12.829,0.0257 12.993,0.2227 12.993,0.4627 L12.993,3.0277 C12.993,3.2847 12.782,3.4947 12.525,3.4947 L12.46,3.4947 C12.203,3.4947 11.993,3.2847 11.993,3.0277 L11.993,0.9997 L4,0.9997 L4,14.9997 L12.01,14.9997 L12.003,12.9607 C12.003,12.7027 12.213,12.4927 12.47,12.4927 L12.535,12.4927 Z M11.4390873,4.90355339 L13.5604076,7.02487373 C14.1461941,7.61066017 14.1461941,8.56040764 13.5604076,9.14619408 L11.4390873,11.2675144 C11.2438252,11.4627766 10.9272427,11.4627766 10.7319805,11.2675144 C10.5367184,11.0722523 10.5367184,10.7556698 10.7319805,10.5604076 L12.8533009,8.4390873 C13.048563,8.24382515 13.048563,7.92724266 12.8533009,7.73198052 L10.7319805,5.61066017 C10.5367184,5.41539803 10.5367184,5.09881554 10.7319805,4.90355339 C10.9272427,4.70829124 11.2438252,4.70829124 11.4390873,4.90355339 Z"
+        fill="#df4759"
+      />
+    </svg>
+  )
+}
+
 const getGroups = () => {
   const { resources } = window.Tensei.state
 
@@ -292,12 +309,18 @@ export const SidebarMenu: React.FunctionComponent<SidebarProps> = ({
   children,
   title
 }) => {
+  const { logout } = useAuthStore()
   const { pathname } = useLocation()
   const [groups, setGroups] = useState(getGroups())
   const [close, setClose] = useState(false)
   const { mergePermissions, hasPermission } = useAuthStore()
 
   const onCloseSideBar = () => setClose(!close)
+
+  const onLogout = async () => {
+    await logout()
+    window.location.href = window.Tensei.getPath('auth/login')
+  }
 
   const isActive = (path: string) => pathname.includes(`resources/${path}`)
 
@@ -405,6 +428,14 @@ export const SidebarMenu: React.FunctionComponent<SidebarProps> = ({
         <NavItem>
           <EuiIcon type="help" />
           <EuiText>Help</EuiText>
+        </NavItem>
+        <EuiSpacer size="s" />
+        <NavItem
+          as={Link as any}
+          onClick={onLogout}
+        >
+          <Exit />
+          <EuiText color="danger">Logout</EuiText>
         </NavItem>
       </Footer>
     </Sidebar>
