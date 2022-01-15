3228c001832ecd13578598a2a8a3a09829a4eb16
Ebuka Odini <ebukaodini@gmail.com>
2022-01-15 20:55:46 +0100

    refactor: applied changes requested and tested endpoint

--- packages/cms/plugin/routes/invite-member.ts
+++ packages/cms/plugin/routes/invite-member.ts
@@ -7,7 +7,7 @@ export const inviteMember = route('Invite Member')
   .authorize(({ user }) => !!user)
   .handle(async (request: Request, response: Response) => {
 
-    const { config, manager, body, resources } = request
+    const { config, manager, resources, body, repositories } = request
     const { emitter } = config
 
     try {
@@ -16,18 +16,35 @@ export const inviteMember = route('Invite Member')
         {
           firstName: 'required',
           lastName: 'required',
-          email: 'required|email|unique',
-          adminRoles: 'required|array'
+          email: 'required|email',
+          adminRoles: 'required'
         },
         {
           'firstName.required': 'The first name is required.',
           'lastName.required': 'The last name is required.',
           'email.required': 'The email is required.',
           'email.email': 'Please provide a valid email.',
-          'email.unique': 'An administrator already exists with this email.',
-          'roles.required': 'The role is required.'
+          'adminRoles.required': 'The role is required.'
         }
       )
+
+      // check that email does not exist
+      const emailExists = await manager.findOne(
+        resources.AdminUser.data.pascalCaseName,
+        {
+          email: body.email
+        }
+      )
+
+      if (emailExists?.email) {
+        return response.status(422).json({
+          errors: [{
+            message: 'The email has already been taken.',
+            validation: 'unique',
+            field: 'email'
+          }]
+        })
+      }
     } catch (error) {
       return response.status(422).json({
         errors: error
@@ -36,17 +53,24 @@ export const inviteMember = route('Invite Member')
 
     const inviteCode = crypto.randomBytes(32).toString('hex')
 
+    // verify roles
+    const roles = await repositories.adminRoles().find({
+      id: {
+        $in: body?.adminRoles
+      }
+    })
+
     let payload: any = {
       firstName: body.firstName,
       lastName: body.lastName,
       email: body.email,
       inviteCode: inviteCode,
-      adminRoles: [...body?.roles],
+      adminRoles: [...roles],
       active: false
     }
 
     const admin: User = manager.create(
-      resources.user.data.pascalCaseName,
+      resources.AdminUser.data.pascalCaseName,
       payload
     )
 
@@ -54,6 +78,6 @@ export const inviteMember = route('Invite Member')
 
     emitter.emit('ADMIN_REGISTERED', admin)
 
-    return response.status(204).json(admin)
+    return response.status(200).json(admin)
 
   })
