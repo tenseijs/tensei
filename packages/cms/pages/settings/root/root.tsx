b737a4098a9d5b8f93ffc1eb51d6069d805bbee5
Malik <malikoabdulaziz@gmail.com>
2022-01-25 16:56:06 +0100

    fix: responsiveness

--- packages/cms/pages/settings/root/root.tsx
+++ packages/cms/pages/settings/root/root.tsx
@@ -18,8 +18,12 @@ const StyledLayout = styled.div`
 
 const TabsWrapper = styled.div`
   padding: 20px 40px 0 40px;
-  @media screen and (max-width: 420px){
-    padding 20px 20px 0 20px
+
+  @media screen and (max-width: 420px) {
+    padding 20px 20px 0 20px;
+    .euiTab + .euiTab {
+      margin-left: 10px;
+    }
   }
 `
 
